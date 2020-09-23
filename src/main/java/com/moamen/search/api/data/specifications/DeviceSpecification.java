a26883b0d41c69419f54a87382f23ad0a077dd83
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-23 16:51:06 +0400

    update specification operation for some fields

--- src/main/java/com/moamen/search/api/data/specifications/DeviceSpecification.java
+++ src/main/java/com/moamen/search/api/data/specifications/DeviceSpecification.java
@@ -2,6 +2,8 @@ package com.moamen.search.api.data.specifications;
 
 import com.moamen.search.api.data.entities.Device;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.EqualIgnoreCase;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
@@ -13,11 +15,11 @@ import org.springframework.data.jpa.domain.Specification;
         @Spec(path = "picture", spec = Equal.class),
         @Spec(path = "sim", spec = Equal.class),
         @Spec(path = "resolution", spec = Equal.class),
-        @Spec(path = "release.announceDate", params = "announceDate",spec = Equal.class),
+        @Spec(path = "release.announceDate", params = "announceDate",spec = Like.class),
         @Spec(path = "release.priceEur", params = "priceEur",spec = Equal.class),
-        @Spec(path = "hardware.audioJack", params = "audioJack",spec = Equal.class),
-        @Spec(path = "hardware.gps", params = "gps",spec = Equal.class),
-        @Spec(path = "hardware.battery", params = "battery",spec = Equal.class)
+        @Spec(path = "hardware.audioJack", params = "audioJack",spec = EqualIgnoreCase.class),
+        @Spec(path = "hardware.gps", params = "gps",spec = EqualIgnoreCase.class),
+        @Spec(path = "hardware.battery", params = "battery",spec = EqualIgnoreCase.class)
 })
 public interface DeviceSpecification extends Specification<Device> {
 }
