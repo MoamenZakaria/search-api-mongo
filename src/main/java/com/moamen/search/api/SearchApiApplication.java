ecf7112ba8259f1bdcd68d240fb9ca6a1291055f
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-23 16:17:48 +0400

    add custom args resolvers

--- src/main/java/com/moamen/search/api/SearchApiApplication.java
+++ src/main/java/com/moamen/search/api/SearchApiApplication.java
@@ -1,10 +1,22 @@
 package com.moamen.search.api;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class SearchApiApplication {
+public class SearchApiApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SearchApiApplication.class, args);
