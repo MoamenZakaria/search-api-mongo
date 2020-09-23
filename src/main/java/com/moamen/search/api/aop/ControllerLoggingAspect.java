d034677603876d6a4f96fc05d775e6f7c8f263e8
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-23 15:13:59 +0400

    add AOP aspects handlers for service,controller and repository

---
+++ src/main/java/com/moamen/search/api/aop/ControllerLoggingAspect.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moamen.search.api.aop;
+
+
+import com.moamen.search.api.constant.Defines;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author mohamed.abdelsalam
+ */
+@Component
+@Aspect
+@Slf4j
+public class ControllerLoggingAspect {
+
+    @Around(Defines.ASPECT_POINT_CUTS.CONTROLLERS)
+    public Object controllerAroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Object obj = null;
+        String methodName = signature.getMethod().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        log.info("Controller::" + className + "." + methodName + " Requested");
+        try {
+            obj = joinPoint.proceed();
+        } finally {
+            if (obj instanceof ResponseEntity) {
+                ResponseEntity response = (ResponseEntity) obj;
+                log.info("Controller::" + className + "." + methodName + " Responded With Code (" + response.getStatusCode() + ") in [" + (System.currentTimeMillis() - startTime) + "] ms");
+            }
+        }
+        return obj;
+    }
+
+}
---
+++ src/main/java/com/moamen/search/api/aop/RepositoryLoggingAspect.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moamen.search.api.aop;
+
+
+import com.moamen.search.api.constant.Defines;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+@Slf4j
+public class RepositoryLoggingAspect {
+
+    @Around(Defines.ASPECT_POINT_CUTS.REPOSITORY)
+    public Object serviceAroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Object obj = null;
+        String methodName = signature.getMethod().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        log.info("Repository::" + className + "." + methodName + " Called");
+        try {
+            obj = joinPoint.proceed();
+        } finally {
+            log.info("Repository::" + className + "." + methodName + " Finished in [" + (System.currentTimeMillis() - startTime) + "] ms");
+        }
+        return obj;
+    }
+
+    @AfterThrowing(pointcut = Defines.ASPECT_POINT_CUTS.SERVICES, throwing = "error")
+    public void serviceAfterThrowingAdvice(JoinPoint joinPoint, Throwable error) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getMethod().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String errorID = String.valueOf(System.currentTimeMillis());
+        log.info("Repository::" + className + "." + methodName + " Throws Error, Error ID : " + errorID);
+        log.error("Repository::" + className + "." + methodName + " Throws Error With ID (" + errorID + ")-> ", error);
+    }
+
+}
