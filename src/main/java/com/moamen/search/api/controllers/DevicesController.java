bdd0ef433576733b87d7a9711054f99b8a883b0f
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-23 19:10:36 +0400

    add search api docs

--- src/main/java/com/moamen/search/api/controllers/DevicesController.java
+++ src/main/java/com/moamen/search/api/controllers/DevicesController.java
@@ -3,9 +3,9 @@ package com.moamen.search.api.controllers;
 import com.moamen.search.api.data.dto.DeviceDto;
 import com.moamen.search.api.data.specifications.DeviceSpecification;
 import com.moamen.search.api.services.DevicesService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
@@ -18,7 +18,7 @@ public class DevicesController {
     public DevicesController(DevicesService devicesService) {
         this.devicesService = devicesService;
     }
-
+    @Operation(summary = "Filter mobiles based on there props like (brand,priceEur,announceDate..etc)",parameters ={})
     @GetMapping("/search")
     @ResponseStatus(code = HttpStatus.OK)
     public List<DeviceDto> findAll(DeviceSpecification specs) {
