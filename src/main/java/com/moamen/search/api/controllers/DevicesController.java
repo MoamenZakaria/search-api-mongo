925b81a0939ba493d257cfc62a936cd4179d15f2
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-23 16:19:32 +0400

    add find all endpoint with filters

--- src/main/java/com/moamen/search/api/controllers/DevicesController.java
+++ src/main/java/com/moamen/search/api/controllers/DevicesController.java
@@ -1,7 +1,12 @@
 package com.moamen.search.api.controllers;
 
+import com.moamen.search.api.data.dto.DeviceDto;
+import com.moamen.search.api.data.specifications.DeviceSpecification;
 import com.moamen.search.api.services.DevicesService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -14,5 +19,9 @@ public class DevicesController {
         this.devicesService = devicesService;
     }
 
-
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<DeviceDto> findAll(DeviceSpecification specs) {
+        return devicesService.findAll(specs);
+    }
 }
