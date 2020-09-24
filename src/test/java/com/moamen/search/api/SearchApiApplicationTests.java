5c5687e7e5993a7f3f7338d0634651ea28a0931e
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-09-24 15:13:21 +0400

    add unit testing for main controller and application

--- src/test/java/com/moamen/search/api/SearchApiApplicationTests.java
+++ src/test/java/com/moamen/search/api/SearchApiApplicationTests.java
@@ -1,10 +1,13 @@
 package com.moamen.search.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class SearchApiApplicationTests {
+@ExtendWith(SpringExtension.class)
+public class SearchApiApplicationTests {
 
     @Test
     void contextLoads() {
