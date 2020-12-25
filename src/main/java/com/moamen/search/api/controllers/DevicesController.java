ff4308e60c1645410171893e49a6f70b7551b0b7
moamen.zakaria <moamen.zakaria.z@gmail.com>
2020-12-25 18:50:58 +0400

    change search endpoint params from specification with custom resolver to normal query param

--- src/main/java/com/moamen/search/api/controllers/DevicesController.java
+++ src/main/java/com/moamen/search/api/controllers/DevicesController.java
@@ -1,11 +1,15 @@
 package com.moamen.search.api.controllers;
 
 import com.moamen.search.api.data.dto.DeviceDto;
-import com.moamen.search.api.data.specifications.DeviceSpecification;
+import com.moamen.search.api.data.dto.SearchCriteria;
 import com.moamen.search.api.services.DevicesService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
@@ -20,14 +24,13 @@ public class DevicesController {
     }
 
     /**
-     *
-     * @param specs
+     * @param searchCriteria query param filters
      * @return List<DeviceDto>
      */
-    @Operation(summary = "Filter mobiles based on there props like (brand,priceEur,announceDate..etc)",parameters ={})
+    @Operation(summary = "Filter mobiles based on there props like (brand,priceEur,announceDate..etc)", parameters = {})
     @GetMapping("/search")
     @ResponseStatus(code = HttpStatus.OK)
-    public List<DeviceDto> findAll(DeviceSpecification specs) {
-        return devicesService.findAll(specs);
+    public List<DeviceDto> findAll(SearchCriteria searchCriteria) {
+        return devicesService.findAll(searchCriteria);
     }
 }
